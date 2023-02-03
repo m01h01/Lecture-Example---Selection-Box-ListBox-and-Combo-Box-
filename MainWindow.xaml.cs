33416a09d4082fd1c0e6e29748772fb8b97a941f
m01h01 <mheang01@student.rtc.edu>
2023-02-03 00:12:00 -0800

    Selection Box

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,10 +19,97 @@ namespace Lecture_Example___Selection_Box___ListBox_and_Combo_Box__
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
+    { 
+        List <Student> students= new List<Student>();
+       Student selectedStudent = null;
         public MainWindow()
         {
             InitializeComponent();
+            Preload();
+            DisplayToListbox();
+            DisplayToCombobox();
+
         }
+
+        private void btAddToList_Click(object sender, RoutedEventArgs e)
+        {
+            string userInput = tbToAdd.Text;
+            lbDisplay.Items.Add(userInput);
+           
+        }
+        public void Preload()
+        {
+            students.Add(new Student("Will", "Cram", 100, 102));
+            students.Add(new Student("Finley", "Deck", 120, 112));
+            students.Add(new Student("Sasha", "Gram", 90, 114));
+            students.Add(new Student("Carson", "Luim", 130, 100));
+            students.Add(new Student("Jennifer", "Mill", 99, 102));
+        }
+        public void DisplayToListbox()
+        {
+            lbDisplay.Items.Clear();
+            for (int i = 0; i < students.Count; i++)
+            {
+                lbDisplay.Items.Add(students[i]);
+            }
+        
+        }
+        public void DisplayToCombobox()
+        {
+            cbDisplay.Items.Clear();
+            for (int i = 0; i < students.Count; i++)
+            {
+
+               cbDisplay.Items.Add(students[i]);
+
+            }
+
+        }
+
+        private void btnDisplaySelectedStu_Click(object sender, RoutedEventArgs e)
+        {
+            int selectedIndex = lbDisplay.SelectedIndex;
+            if (selectedIndex < 0)
+            {
+                MessageBox.Show("Please Select a Name from List Box");
+            }
+            else 
+            {
+                selectedStudent = students[selectedIndex];
+                DisplayStudentInformation(selectedStudent);
+
+            }
+        }
+        public void DisplayStudentInformation(Student student)
+        {
+            tbFirstName.Text = student.FirstName;
+            tbLastName.Text = student.LastName;
+            tbCSIGrade.Text = student.CSIGrade.ToString();
+            tbGenEdGrade.Text = student.GenEdGrade.ToString();
+        }
+
+        private void btnAddStudent_Click(object sender, RoutedEventArgs e)
+        {
+            string fName = tbFirstName.Text;
+            string lName = tbLastName.Text;
+            int csi = int.Parse(tbCSIGrade.Text);
+            int gened = int.Parse(tbGenEdGrade.Text);
+
+            Student newStudent = new Student(fName,lName, csi, gened);
+            students.Add(newStudent);
+            DisplayToListbox();
+            DisplayToCombobox();
+        }
+
+        private void btnRemoveSelectedStudent_Click(object sender, RoutedEventArgs e)
+        {
+            int selectedIndex = lbDisplay.SelectedIndex;
+            selectedStudent = students[selectedIndex];
+            students.Remove(selectedStudent);
+
+            DisplayToListbox();
+            DisplayToCombobox();
+        }
+
     }
 }
---
+++ Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_Example___Selection_Box___ListBox_and_Combo_Box__
+{
+    public class Student
+    {
+        public string FirstName;
+        public string LastName;
+        public int CSIGrade;
+        public int GenEdGrade;
+
+        public Student(string firstName, string lastName, int cSIGrade, int genEdGrade)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            CSIGrade = cSIGrade;
+            GenEdGrade = genEdGrade;
+        }
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName}";
+        }
+    }
+}
